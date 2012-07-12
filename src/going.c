d41dd9c4404f07c11da5dee690a58ef3606ed11a
Antoine d'Otreppe <a.dotreppe@aspyct.org>
2012-07-12 23:31:43 +0200

    Fixed the compilation error, and now creating and listening to the socket

--- src/going.c
+++ src/going.c
@@ -4,7 +4,8 @@
 #include <unistd.h>
 #include <syslog.h>
 #include <sys/socket.h>
-#include <strings.h>
+#include <string.h>
+#include <sys/un.h>
 
 #include "daemonize.h"
 
@@ -13,7 +14,14 @@ struct going_data {
 	char *socket_path;
 };
 
+struct client {
+	int fd;
+	struct sockaddr addr;
+	socklen_t addrlen;
+};
+
 void going(void *data);
+void handle_client(struct client *client);
 
 int main(int argc, char *argv[]) {
 	struct going_data parameters;
@@ -32,7 +40,7 @@ int main(int argc, char *argv[]) {
 void going(void *data) {
 	struct going_data *parameters = (struct going_data *)data;
 	int server_fd;
-	sockaddr_un server_address;
+	struct sockaddr_un serveraddr;
 
 	if (chdir(parameters->directory_path) < 0) {
 		syslog(LOG_CONS, "Could not change directory, exiting");
@@ -49,10 +57,38 @@ void going(void *data) {
 	dup2(fileno, STDERR_FILENO);
 
 	printf("Creating server");
-	unlink(parameters.socket_path);
+	unlink(parameters->socket_path);
 	server_fd = socket(AF_UNIX, SOCK_STREAM, 0);
 
-	server_address.sun_family = AF_UNIX;
-	strcopy(server_address.sun_path, 
+	memset(&serveraddr, 0, sizeof(serveraddr));
+	serveraddr.sun_family = AF_UNIX;
+	strcpy(serveraddr.sun_path, parameters->socket_path);
+
+	int rc = bind(server_fd, (struct sockaddr*)&serveraddr, SUN_LEN(&serveraddr));
+	if (rc < 0) {
+		perror("Could not bind()");
+		exit(EXIT_FAILURE);
+	}
+
+	if (listen(server_fd, 10) < 0) {
+		perror("Could not listen");
+		exit(EXIT_FAILURE);
+	}
+
+	while (1) {
+		struct client *client = malloc(sizeof(struct client));
+		client->fd = accept(server_fd, &client->addr, &client->addrlen);
+
+		if (client->fd < 0) {
+			perror("Could not accept(), dropping client");
+		}
+		else {
+			handle_client(client);
+		}
+	}
+}
+
+void handle_client(struct client *client) {
+	send(client->fd, "Hello there!\n", 12, 0);
 }
 
